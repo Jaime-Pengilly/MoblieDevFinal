879272c1c2e40da2b149300827e9e6d4b1c01287
Jaime-Pengilly <116103802+Jaime-Pengilly@users.noreply.github.com>
2024-04-29 08:16:05 -0500

    Initial Commit

---
+++ MoblieDevFinal/MoblieDevFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoblieDevFinalApp.swift
+//  MoblieDevFinal
+//
+//  Created by PENGILLY, JAIME on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MoblieDevFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
